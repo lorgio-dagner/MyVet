708cd4e9b3fecde037ea0100ce6a4f04cbc4bc5e
Lorgio Dagner <lorgio.dagner@gmail.com>
2020-06-15 17:14:33 -0400

    APP

---
+++ MyVet.Prism/MyVet.Prism/ViewModels/PetsPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using MyVet.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MyVet.Prism.ViewModels
+{
+    // Aqui Cambiamos "BindableBase" por ViewModelBase este cmabio se hace a todas la ViewModel
+    public class PetsPageViewModel : ViewModelBase
+    {
+        private OwnerResponse _owner;
+        private ObservableCollection<PetResponse> _pets;
+
+
+        // y al heredar de la ViewModelBase necesitamos inyectar el INavigationServer
+        public PetsPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Pets";
+
+        }
+        public ObservableCollection<PetResponse> Pets
+        {
+            get => _pets;
+            set => SetProperty(ref _pets, value);
+
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if(parameters.ContainsKey("owner"))
+            {
+                _owner = parameters.GetValue<OwnerResponse>("owner");
+                Title = $"Pets of: {_owner.FullName}";
+                Pets = new ObservableCollection<PetResponse>(_owner.Pets);
+
+            }
+        }
+    }
+}
--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -32,10 +32,10 @@ namespace MyVet.Web.Data.Entities
 
         
 
-        //TODO: replace the correct URL for the image
+        //TODO: replace the correct URL for the image ya lo hice 16/05/2020
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
             ? null
-            : $"https://TDB.azurewebsites.net{ImageUrl.Substring(1)}";
+            : $"https://myvetweb2020.azurewebsites.net{ImageUrl.Substring(1)}";
 
         [Display(Name = "Born")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}")]
